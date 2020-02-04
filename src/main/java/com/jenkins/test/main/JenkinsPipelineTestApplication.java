7000db7ceab643573c965ac916be805d2dd50b8c
sushil <sushilent1@gmail.com>
2020-02-04 20:31:41 +0530

    Initial commit

---
+++ src/main/java/com/jenkins/test/main/JenkinsPipelineTestApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.test.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/test/jenkinspipelinetest/JenkinsPipelineTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jenkins.test.jenkinspipelinetest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsPipelineTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
