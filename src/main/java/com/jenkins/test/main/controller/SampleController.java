3cf746bc28e7526c406b194b7ac3c887fa9d3294
sushil <sushilent1@gmail.com>
2020-02-05 12:30:48 +0530

    updated src

--- src/main/java/com/jenkins/test/main/controller/SampleController.java
+++ src/main/java/com/jenkins/test/main/controller/SampleController.java
@@ -1,4 +1,4 @@
-package com.jenkins.test.controller;
+package com.jenkins.test.main.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
